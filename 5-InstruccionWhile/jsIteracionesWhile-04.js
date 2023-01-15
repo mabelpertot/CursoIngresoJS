0e63e2123a19d04353d8174b357bbc1c0d81b2ad
mabelpertot <mabelgem77@gmail.com>
2023-01-15 18:40:00 -0300

    Ejercicio4WHILE

--- 5-InstruccionWhile/jsIteracionesWhile-04.js
+++ 5-InstruccionWhile/jsIteracionesWhile-04.js
@@ -1,10 +1,11 @@
 
 {
-	var numero=prompt("ingrese un número entre 0 y 10.");
+	var numero
+	numero=prompt("ingrese un número entre 0 y 10.");
 
 	while (numero!='10'){
-	alert('Dato Invalido');
-	numero=prompt("ingrese un número entre 0 y 10.");
-    }
-	alert('Dato Valido');	
+		alert('Dato Invalido');
+		numero=prompt("ingrese un número entre 0 y 10.");
+    }	
 }
+txtIdNumero.value=num;
