bff89bd85372fed3b228e8256ac310fae98f029c
mabelpertot <mabelgem77@gmail.com>
2023-01-21 13:29:18 -0300

    Realizado por Classroom

--- 1-EntradaSalida/jsEntradaSalida-06.js
+++ 1-EntradaSalida/jsEntradaSalida-06.js
@@ -3,8 +3,8 @@ Debemos lograr tomar Los numeros por ID ,
 transformarlos a enteros (parseInt) y Sumarlos.
 mostrar el resulto por medio de "ALERT"*/
 function sumar()
-{
-var uno;
+
+/*var uno;
 var dos;
 var igual;
 
@@ -12,5 +12,16 @@ uno=txtIdNumeroUno.value;
 dos=txtIdNumeroDos.value;
 igual=parseInt(uno) + parseInt (dos)	
 
-alert("El resultado es " + igual);
+alert("El resultado es " + igual);*/
+
+{
+    let num1;
+    let num2;
+    let resultado;
+
+    num1 = parseInt(document.getElementById ("txtIdNumeroUno").value);
+    num2 = parseInt(document.getElementById ("txtIdNumeroDos").value);
+    resultado = num1 + num2;
+
+    alert("El resultado es " + resultado);
 }
