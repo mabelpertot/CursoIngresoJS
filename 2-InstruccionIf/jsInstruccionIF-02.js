81404d564ad4a1b277720a9da281c3a993994fd2
mabelpertot <mabelgem77@gmail.com>
2023-01-26 00:02:32 -0300

    Realizado por classroom

--- 2-InstruccionIf/jsInstruccionIF-02.js
+++ 2-InstruccionIf/jsInstruccionIF-02.js
@@ -1,11 +1,19 @@
 function mostrar()
 {
-	var edad;
+	/*var edad;
 
 	edad=parseInt(txtIdEdad.value);
 
 	if (edad>='18'){
 		alert("✅ Es mayor de edad");
+	}*/
+
+	let edad;
+	edad = parseInt(document.getElementById("txtIdEdad").value);
+
+	if(edad >= 18){
+		alert("Es mayor de edad");
 	}
+	  
 }
 
