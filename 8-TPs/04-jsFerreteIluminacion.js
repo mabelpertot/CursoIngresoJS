6b07843b99fc1ae68b6bc3879b2f8ee235ed63ef
mabelpertot <mabelgem77@gmail.com>
2023-01-28 11:55:36 -0300

    Realizado por classroom

--- 8-TPs/04-jsFerreteIluminacion.js
+++ 8-TPs/04-jsFerreteIluminacion.js
@@ -10,5 +10,63 @@ E.	Si el importe final con descuento suma más de $120  se debe sumar un 10% de 
  */
 function CalcularPrecio () 
 {
- 	
+    const PRECIO = 35;
+    let cantidad;
+    let marca;
+    let descuento;
+    let importeFinal;
+    let iibb;
+    let precioConDescuento;
+    
+    cantidad = parseInt(document.getElementById("txtIdCantidad").value);
+    marca = document.getElementById("Marca").value;
+
+    switch(cantidad){
+        case 1:
+        case 2:
+            descuento = 0;
+            break;
+        case 3:
+            if(marca == "ArgentinaLuz"){
+            descuento = 15;
+            }
+            else if(marca == "FelipeLamparas"){
+                descuento = 10;
+            }
+            else{
+                 descuento = 5;
+            }
+            break;
+         case 4:
+            if(marca == "ArgentinaLuz" || marca == "FelipeLamparas"){
+                descuento = 25;
+            }
+            else{
+                descuento = 20;
+            }
+            break;
+         case 5:
+            if (marca == "ArgentinaLuz"){
+            descuento = 40;
+            }
+            else{
+                descuento = 30;
+            }
+            break;
+        default:
+            descuento = 50;
+    } 
+    precioConDescuento = PRECIO - PRECIO * descuento /100;
+    document.getElementById("txtIdprecioDescuento").value = precioConDescuento;
+
+    importeFinal = precioConDescuento * cantidad;
+    if(importeFinal > 120){
+        iibb = importeFinal *10 / 100;
+        importeFinal += iibb;
+       
+        alert(`Importe a pagar $ ${importeFinal}. Usted pago $ ${iibb} de Ingresos Brutos`);
+    }
+    else{
+        alert(`Importe a pagar $ ${importeFinal}.`);
+}
 }
