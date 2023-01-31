a771dc692be7a2cab427f71b9c86f3dcd597a960
mabelpertot <mabelgem77@gmail.com>
2023-01-30 23:46:29 -0300

    Realizado por classroom

--- 5-InstruccionWhile/jsIteracionesWhile-08.js
+++ 5-InstruccionWhile/jsIteracionesWhile-08.js
@@ -3,17 +3,35 @@ Al presionar el botón pedir  números  hasta que el usuario quiera,
 sumar los que son positivos y multiplicar los negativos.*/
 function mostrar()
 {
-	var contador;
-	var respuesta;
-	var sumaPositivos;
-	var multiplicacionNegativos;
-	contador=0;
-	sumaPositivos=0;
-	multiplicacionNegativos=1;
-	respuesta='si';
+	//Declarar e inicializar de corresponder, variables.
+	let seguir;
+	let numero;
+	let positivos = 0;
+	let negativos = 1;
+	let flag = 0; // let contadorNegativos = 0; podria ser tambien una variable.
 
+	do{
+		//Pedir los numeros ---> dentro del bucle x.
+        numero = parseInt(prompt("Ingrese un numero: "));
 
-	txtIdSuma.value=sumaPositivos;
-	txtIdProducto.value=multiplicacionNegativos;
+        //Analizar el signo de cada número ---> dentro del bucle x.
+		//Acumulamos el numero en el acumulador correspondiente ---> dentro del bucle x.
+		if(numero >= 0){
+			positivos = positivos + numero;
+		}
+		else{
+			flag = 1; /* contadorNegtivos++; flag Determina si ingreso al flujo del programa*/
+			negativos = negativos * numero;
+		}
 
-}//FIN DE LA FUNCIÓN
+		seguir = prompt("Quiere ingresar otro numero?");
+
+	}while(seguir == 's');
+
+	//Mostrar los resultados ---> fuera del bucle x.
+    if(flag == 0){ //(contadorNegativos == 0){
+		negativos = 0;
+    }
+	document.getElementById("txtIdProducto").value = negativos;
+	document.getElementById("txtIdSuma").value = positivos;	
+}
