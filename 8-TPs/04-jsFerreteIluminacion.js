1cbc778c836d8dc8319fb62a9a9b05539c525896
mabelpertot <mabelgem77@gmail.com>
2023-02-07 20:57:30 -0300

    Realizado por Class

--- 8-TPs/04-jsFerreteIluminacion.js
+++ 8-TPs/04-jsFerreteIluminacion.js
@@ -8,7 +8,7 @@ E.	Si el importe final con descuento suma más de $120  se debe sumar un 10% de 
  ”Usted pago X de IIBB.”, siendo X el impuesto que se pagó. 
 
  */
-function CalcularPrecio () 
+function CalcularPrecio ()
 {
     const PRECIO = 35;
     let cantidad;
@@ -70,3 +70,4 @@ function CalcularPrecio ()
         alert(`Importe a pagar $ ${importeFinal}.`);
 }
 }
+
