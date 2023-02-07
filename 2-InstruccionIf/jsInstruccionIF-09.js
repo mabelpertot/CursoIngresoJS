ed93417a6c564446ed130e64ac8936162b61e490
mabelpertot <mabelgem77@gmail.com>
2023-02-06 21:52:39 -0300

    Realizado en classroom

--- 2-InstruccionIf/jsInstruccionIF-09.js
+++ 2-InstruccionIf/jsInstruccionIF-09.js
@@ -1,7 +1,5 @@
 function mostrar()
 {
-
-
 	/*var numero=Math.floor(Math.random()*11);
 
 	/*alert(numero);*/
--- 2-InstruccionIf/jsInstruccionIF-10.js
+++ 2-InstruccionIf/jsInstruccionIF-10.js
@@ -1,6 +1,21 @@
 function mostrar()
 {
-  /*var numero=Math.floor(Math.random()*11);
+  let nota;
+
+  nota = Math.round(Math.random() * 9 +1);
+
+  if(nota > 9){
+  alert("EXCELENTE Nota: "+ nota);
+  }
+  else if(nota > 4){
+  alert("APROBÓ Nota: " + nota);
+  }
+   else{
+  alert("Vamos, la proxima se puede Nota: " + nota)
+
+ }
+}
+ /*var numero=Math.floor(Math.random()*11);
   console.log(numero);
 
   if (numero<'4'){
@@ -9,21 +24,4 @@ function mostrar()
     alert("APROBÓ");
   }else{
     alert('EXCELENTE');
-  }*/
-
-     let nota;
-
-     nota = Math.round(Math.random() * 9 +1);
-
-     if(nota > 9){
-       alert("EXCELENTE Nota: "+ nota);
-      }
-      else if(nota > 4){
-        alert("APROBÓ Nota: " + nota);
-      }
-        else{
-          alert("Vamos, la proxima se puede Nota: " + nota)
-
-        }
-      }
-    
+  }*/    
